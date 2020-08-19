91055ce5a79265a7058d31cbbe052d575d9a7ee7
fazalfarhan01 <45929854+fazalfarhan01@users.noreply.github.com>
2020-08-19 14:59:13 +0530

    Update ExtractAndReplaceXML.py
    Added Version info and made more interactive

--- ExtractAndReplaceXML.py
+++ ExtractAndReplaceXML.py
@@ -1,12 +1,34 @@
+#!/usr/bin/python3
+
+#******************************VERSION INFO******************************#
+# Version 1.2
+# Written by Mohamed Farhan Fazal
+# Needed Binaries:  1. urllib
+#                   2. bs4
+#                   3. re (Regular Expression)
+#                   4. io
+#******************************VERSION INFO******************************#
+
+
 from urllib import request
 from bs4 import BeautifulSoup
 import re
 import io
 
+print("""*****************************************************************
 
-url = input("Enter the URL to the Sitemaps Index\nLeave blank for using default:\n")
+                            Hola Amigos!!
+
+*****************************************************************""")
+
+url = input("\n\nEnter the URL to the Sitemaps Index\nLeave blank for using default:\n\n")
 if len(url) < 1:
     url = "http://ftm.ddns.net/sitemap_index.xml"
+    print("Using Default URL:",url)
+else:
+    print("Using URL:",url)
+
+print("\n\n")
 
 handle = request.urlopen(url).read()
 soup = BeautifulSoup(handle, 'html.parser')
@@ -17,16 +39,26 @@ for sitemaps in sitemapTags:
     if sitemaps.string.endswith(".xml"):
         sitemapURLList.append(sitemaps.string)
 
-wantToReplace = input("Do you want to replace text in files?\ntype 'y' for yes or 'n' for no: ")
-if wantToReplace == 'y':
+wantToReplace = input("Do you want to replace text in files?\ntype 'y' for yes or 'n' or leave blank for no: \n\n")
+if wantToReplace == ('y' or 'Y'):
     textToReplace = input("enter the text to replace: ")
     textToBeReplacedWith = input("enter the text to be replaced with: ")
-
+wantHttps = input("\n\nWant to change \"http\" to \"https\" for custom URL?: \ntype 'y' for yes or 'n' or leave blank for no: \n")
+print("\n\n")
 for linksFromSitemaps in sitemapURLList:
-    if wantToReplace == "y":
-        textData = request.urlopen(linksFromSitemaps).read().decode().replace(textToReplace,textToBeReplacedWith)
+    if wantToReplace == ('y' or 'Y'):
+        if wantHttps == ('y' or 'Y'):
+            textData = request.urlopen(linksFromSitemaps).read().decode().replace(textToReplace,textToBeReplacedWith).replace("http:"+textToBeReplacedWith,"https:"+textToBeReplacedWith)
+        else:
+            textData = request.urlopen(linksFromSitemaps).read().decode().replace(textToReplace,textToBeReplacedWith)
     else:
         textData = request.urlopen(linksFromSitemaps).read().decode()
     with io.open(linksFromSitemaps.split("/")[-1], "w", encoding="utf-8") as fileToBeWritten:
         fileToBeWritten.write(textData)
         fileToBeWritten.close()
+
+print("""*****************************************************************
+
+                        Voila!! Done!!
+
+*****************************************************************""")
