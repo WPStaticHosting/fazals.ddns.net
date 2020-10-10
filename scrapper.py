474f632b85b5dcc1f3773e710d610fa473b16b54
fazalfarhan01 <45929854+fazalfarhan01@users.noreply.github.com>
2020-10-10 11:11:57 +0530

    Revert "Adding rough estimates"
    This reverts commit 9079d97c29251b8f0761e0bfaf44a3c3a3af98dd.

--- scrapper.py
+++
@@ -1,5 +0,0 @@
-from src.download import download
-
-url = "https://fazals.ddns.net/wp-content/uploads/2020/08/flower-729512_1920-1.jpg"
-# url = "https://fazals.ddns.net/class/ec550-class-11/"
-download(url)
